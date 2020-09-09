dfa1be5100f695f3b392d481b8adb05daf111c9b
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2020-09-09 10:54:37 +0530

    added classes

--- proxy_bot/constants.py
+++ proxy_bot/constants.py
@@ -3,7 +3,7 @@ schedule = {
                 'Monday':{
                     '09:00':['DBMS LAB','https://classroom.google.com/u/0/c/MTM4OTIzODA3NDIy'],
                     '11:00':['DBMS','https://classroom.google.com/u/0/c/MTM4OTIzODA3NDIy'],
-                    #'13:00':['Entrepenureship],
+                    '13:00':['Entrepenureship','https://classroom.google.com/u/2/c/MTMyMDg1Mjg1OTU5'],
                     '14:00':['ADA','https://classroom.google.com/u/0/c/MTMxODE1MTg5ODY3'],
                     '15:00':['Modelling & Simulation','https://classroom.google.com/u/0/c/MTU4Mzk0MzM0OTg0'],
                     '16:00':['Compiler Design','https://classroom.google.com/u/0/c/MTUzNjk3NjM5OTkz'],
@@ -12,14 +12,14 @@ schedule = {
                 'Tuesday':{
                     '10:00':['DBMS','https://classroom.google.com/u/0/c/MTM4OTIzODA3NDIy'],
                     '11:00':['Modelling & Simulation','https://classroom.google.com/u/0/c/MTU4Mzk0MzM0OTg0'],
-                    #'13:00':'CSE 3rd year LAB (CSD-318)',
-                    #'14:00':'CSE 3rd year LAB (CSD-318)',
+                    '13:00':['Entrepenureship','https://classroom.google.com/u/2/c/MTMyMDg1Mjg1OTU5'],
+                    '14:00':['Communication Engg','https://classroom.google.com/u/2/c/MTU5OTM5OTI2NzQy'],
                     '15:00':['ADA','https://classroom.google.com/u/0/c/MTMxODE1MTg5ODY3'],
                     '16:00':['Compiler Design','https://classroom.google.com/u/0/c/MTUzNjk3NjM5OTkz'],
 
                 },
                 'Wednesday':{
-                   # '10:00':'CSE 3rd year LAB (CSD-318)',
+                    '10:00':['Communication Engg','https://classroom.google.com/u/2/c/MTU5OTM5OTI2NzQy'],
                     '11:00':['DBMS','https://classroom.google.com/u/0/c/MTM4OTIzODA3NDIy'],
                     '12:00':['Modelling & Simulation','https://classroom.google.com/u/0/c/MTU4Mzk0MzM0OTg0'],
                     '15:00':['ADA','https://classroom.google.com/u/0/c/MTMxODE1MTg5ODY3'],
@@ -28,17 +28,17 @@ schedule = {
                 },
                 'Thursday':{
                     '10:00':['Modelling & Simulation Lab','https://classroom.google.com/u/0/c/MTU4Mzk0MzM0OTg0'],
-                    '11:00':['DBMS','https://classroom.google.com/u/0/c/MTM4OTIzODA3NDIy'],
-                  #  '13:00':'CSE 3rd year LAB (CSD-318)',
+                    '11:00':['Communication Engg','https://classroom.google.com/u/2/c/MTU5OTM5OTI2NzQy'],
+                    '13:00':['Entrepenureship','https://classroom.google.com/u/2/c/MTMyMDg1Mjg1OTU5'],
                     '16:00':['Compiler Design','https://classroom.google.com/u/0/c/MTUzNjk3NjM5OTkz'],
                     '17:00':['Compiler Design Lab','https://classroom.google.com/u/0/c/MTM5NjU4MTU5NTMz'],
 
                 },
                 'Friday':{
                     '10:00':['Modelling & Simulation','https://classroom.google.com/u/0/c/MTU4Mzk0MzM0OTg0'],
-                  #  '11:00':['DBMS','https://classroom.google.com/u/0/c/MTM4OTIzODA3NDIy'],
-                  #  '13:00':'CSE 3rd year LAB (CSD-318)',
-                    '14:00':'CSE 3rd year LAB (CSD-318)',
+                    '11:00':['DBMS','https://classroom.google.com/u/0/c/MTM4OTIzODA3NDIy'],
+                    '13:00':['Entrepenureship','https://classroom.google.com/u/2/c/MTMyMDg1Mjg1OTU5'],
+                    '14:00':['Communication Engg','https://classroom.google.com/u/2/c/MTU5OTM5OTI2NzQy'],
                     '15:00':['ADA','https://classroom.google.com/u/0/c/MTMxODE1MTg5ODY3'],
                 }
 
